a600ba938ecc05e4348f2f409901ade888b1ee16
Diego Gomes <dgomes@live.com>
2024-06-25 13:59:32 -0500

    Removed comment from CyberShuttle pkgIndex.tcl

--- TclTk-gui/pkgIndex.tcl
+++ TclTk-gui/pkgIndex.tcl
@@ -1,4 +1,3 @@
-#package ifneeded cybershuttle 0.8 "set env(CYBERSHUTTLEDIR) [list $dir]; [list source [file join $dir CyberShuttle.tcl] [list source [file join $dir CyberShuttle_submit.tcl] [file join $dir CyberShuttle_functions.tcl]] ]"
 package ifneeded cybershuttle 0.8 "set env(CYBERSHUTTLEDIR) [list $dir]; [list source [file join $dir CyberShuttle.tcl]]"
 package ifneeded cybershuttlesubmit 0.8 "set env(CYBERSHUTTLEDIR) [list $dir]; [list source [file join $dir CyberShuttle_submit.tcl]]"
 
